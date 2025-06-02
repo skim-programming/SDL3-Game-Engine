35a1c05c4fd6ebe74395aff444da66bf82787059
Minsoo Kim <skimprogramming@gmail.com>
2025-06-01 20:37:52 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <SDL3/SDL.h>
+#include "window.h"
+
+int main() {
+    easyWindow window("test");
+    SDL_Renderer* renderer = window.getRenderer();
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT)
+                running = false;
+        }
+
+        // Clear screen (black)
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw a red rectangle
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_FRect rect = { 100, 100, 200, 150 };
+        SDL_RenderFillRect(renderer, &rect);
+
+        // Update screen
+        SDL_RenderPresent(renderer);
+    }
+
+    window.easyCleanup();
+    return 0;
+}
---
+++ window.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef EASY_WINDOW_H
+#define EASY_WINDOW_H
+
+#include <SDL3/SDL.h>
+#include <string>
+#include <stdexcept>
+
+
+class easyWindow {
+private:
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+
+public:
+	easyWindow(const std::string& windowTitle) {
+		SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+		const SDL_DisplayMode* display = SDL_GetDesktopDisplayMode(SDL_GetPrimaryDisplay());
+		window = SDL_CreateWindow(windowTitle.c_str(), 
+			display->w, 
+			display->h, 
+			SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
+		if (!window) {
+			throw std::runtime_error("Window creation failed: " + std::string(SDL_GetError()));
+		}
+		renderer = SDL_CreateRenderer(window, NULL);
+	}
+
+	SDL_Renderer* getRenderer() {
+		return renderer;
+	}
+
+	SDL_Window* getWindow() {
+		return window;
+	}
+
+	void easyCleanup() {
+		if(renderer)
+		{
+			SDL_DestroyRenderer(renderer);
+			renderer = nullptr;
+		}
+		if(window)
+		{
+			SDL_DestroyWindow(window);
+			window = nullptr;
+		}
+		SDL_Quit();
+	}
+
+	~easyWindow() {
+		easyCleanup();
+	}
+};
+
+#endif
